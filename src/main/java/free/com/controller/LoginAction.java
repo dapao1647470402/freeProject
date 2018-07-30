c7fec339195e2b3011d291a4285b73c2bd5309ae
dapao1647470402 <dapao1647470402@163.com>
2018-07-30 22:20:13 +0800

    about jsp

--- src/main/java/free/com/controller/LoginAction.java
+++ src/main/java/free/com/controller/LoginAction.java
@@ -1,5 +1,15 @@
 package free.com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("login")
 public class LoginAction {
+
+	@RequestMapping("init")
+	public String init() {
+		return "login";
+	}
 
 }
