5c9fee5aae94a050ab2f9206c62dce9305a27809
dapao <dapao1647470402@163.com>
2018-08-02 15:58:37 +0900

    21080802 15:56

--- src/main/java/free/com/controller/LoginController.java
+++ src/main/java/free/com/controller/LoginController.java
@@ -1,11 +1,14 @@
 package free.com.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import free.com.service.LoginService;
+import free.com.utils.CommonConstants;
 
 @Controller
 @RequestMapping("login")
@@ -15,9 +18,17 @@ public class LoginController {
 	private LoginService service;
 
 	@RequestMapping("init")
-	public String init() {
+	public String init(Model model) {
 		service.init();
+		model.addAttribute(CommonConstants.LOGIN_STATUS, CommonConstants.loginStatus.INIT);
+		model.addAttribute("projectResume", "............................");
 		return "login";
+	}
+
+	@RequestMapping("login")
+	public String login(HttpSession session) {
+		session.setAttribute(CommonConstants.USER, "Test01");
+		return "forward:main/init";
 	}
 
 }
