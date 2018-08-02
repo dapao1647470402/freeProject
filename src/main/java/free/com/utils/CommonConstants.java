524df33b0bb870d9a4245add5d7b3c2b2c517d7d
dapao <dapao1647470402@163.com>
2018-08-02 15:56:53 +0900

    21080802 15:56

--- src/main/java/free/com/utils/CommonConstants.java
+++ src/main/java/free/com/utils/CommonConstants.java
@@ -2,8 +2,28 @@ package free.com.utils;
 
 public class CommonConstants {
 
-	public static String USER = "userId";
+	public static final String USER = "userId";
 
-	public static String STR_NULL = "null";
+	public static final String STR_NULL = "null";
 
+	public static final String SUCCESS = "1";
+
+	public static final String FAILURE = "0";
+
+	public static final String LOGIN_STATUS = "loginStatus";
+
+
+
+	private CommonConstants() {
+
+	}
+
+	public static final class loginStatus {
+		private loginStatus() {
+			// TODO 自動生成されたコンストラクター・スタブ
+		}
+		public static final String INIT = "init";
+		public static final String LOGIN = "login";
+		public static final String LOGOUT = "logout";
+	}
 }
