1b2df8cffa9eb1a9f7f4eaf619f9e2cabd35dc57
dapao1647470402 <dapao1647470402@163.com>
2018-08-13 22:03:30 +0800

    20180813 22:03

--- src/main/java/free/com/controller/LoginController.java
+++ src/main/java/free/com/controller/LoginController.java
@@ -1,5 +1,6 @@
 package free.com.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -7,10 +8,15 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import free.com.bean.User;
+import free.com.common.LoggerCommon;
 import free.com.service.LoginService;
 import free.com.utils.CommonConstants;
+import free.com.utils.SystemEnum;
 
 @Controller
 @RequestMapping("login")
@@ -29,9 +35,25 @@ public class LoginController {
 	}
 
 	@RequestMapping("login")
-	public String login(HttpSession session) {
-		session.setAttribute(CommonConstants.USER, "Test01");
-		return "forward:main/init";
+	public String login(HttpSession session, User user, RedirectAttributes redirectModel) {
+		if (StringUtils.isEmpty(user.getAccount()) || StringUtils.isEmpty(user.getPassword())) {
+			LoggerCommon.printLog("☆☆☆☆☆Please Again Login causer By Not input Account Or Password☆☆☆☆☆",
+					SystemEnum.LOG_LEVEL_DEBUG);
+			redirectModel.addFlashAttribute("loginFailure", CommonConstants.FAILURE);
+			return "redirect:/login/init";
+		}
+
+		List<User> userInfo = service.login(user);
+		if (userInfo.size() > 1) {
+			// TODO
+		} else if (userInfo.size() == 0) {
+			redirectModel.addFlashAttribute("loginFailure", CommonConstants.FAILURE);
+			return "redirect:/login/init";
+		} else {
+			session.setAttribute(CommonConstants.USER, userInfo.get(0).getUserId());
+			return "forward:/main/init";
+		}
+		return null;
 	}
 
 }
